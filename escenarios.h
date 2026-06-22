f702db1cf4aaccd4560ee16a02bf609041a9c3aa
Guido Landa <guidofdelanda@gmail.com>
2026-06-22 13:12:06 -0300

    Struct Escenario

---
+++ escenarios.h
@@ -0,0 +1,13 @@
+#ifndef ESCENARIOS_H
+#define ESCENARIOS_H
+
+typedef struct {
+    int id;
+    char nombre[50];
+} Escenario;
+
+int CargaEscenario(Escenario arreglo[], int validos, int dimension);
+void OrdenarEscenario(Escenario arreglo[], int validos);
+void MostrarEscenario(Escenario arreglo[], int validos);
+
+#endif
